8ee6860e79b11e9f7669252492fbcf8fe1916141
s22030 <s22030@std.it-college.ac.jp>
2023-02-02 07:59:00 +0000

    223finish

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,6 +1,7 @@
 import { getPostBySlug } from 'lib/api'
 import Container from 'next-react-website/blog/components/container'
 import PostHeader from 'components/post-header'
+import Image from 'next/image'
 
 export default function Schedule({
     title,
@@ -11,7 +12,20 @@ export default function Schedule({
 }) {
     return(
         <Container>
-            <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+            <article>
+                <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+                <figure>
+                    <Image
+                    src={eyecatch.url}
+                    alt=""
+                    layout="responsive"
+                    width={eyecatch.width}
+                    height={eyecatch.height}
+                    sizes="(min-width: 1152px) 1152px, 100vw"
+                    priority
+                    />
+                </figure>
+            </article>
         </Container>
     )
 }
